92308ee87970ccf75af9990c686aea6f93f432fe
Behzad <saeedibehzad@gmail.com>
2021-10-24 11:15:36 +0330

    install FontAwesome an duse it

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserAlt ,faTrashAlt} from '@fortawesome/free-solid-svg-icons'
+
 const Contact = ({name,email}) => {
     return ( 
         <div>
+            <FontAwesomeIcon icon={faUserAlt} />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
             </div>
+            <FontAwesomeIcon icon={faTrashAlt} />
         </div>
      );
 }
